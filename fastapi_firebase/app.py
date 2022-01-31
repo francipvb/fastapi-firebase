f6d551dde6265c843a368a0b4d3d2e7355dfaedf
Francisco Del Roio <francipvb@hotmail.com>
2022-01-31 10:39:11 -0300

    Added tests and finished firebase sdk initialization.

--- fastapi_firebase/app.py
+++ fastapi_firebase/app.py
@@ -1,7 +1,13 @@
 import typing
-
+import logging
 from fastapi import FastAPI
 from firebase_admin import App, credentials, delete_app, initialize_app
+
+log = logging.getLogger()
+
+
+class NotInitializedError(Exception):
+    pass
 
 
 def _initialize_app(
@@ -45,7 +51,7 @@ def _initialize_app(
     if firebase_options:
         kwargs["options"] = firebase_options
 
-    return initialize_app(name=app_name)
+    return initialize_app(**kwargs)
 
 
 def setup(
@@ -57,12 +63,41 @@ def setup(
     @app.on_event("startup")
     def _setup_app():
         nonlocal _app
-        _app = _initialize_app(**kwargs)
+        try:
+            _app = _initialize_app(app_name=_app_name(app), **kwargs)
+        except Exception as ex:
+            log.exception(
+                "Error while trying to initialize the firebase SDK with provided args.",
+                stack_info=True,
+                exc_info=ex,
+            )
+            raise
+
+        app.dependency_overrides[firebase_app] = lambda: _app
+        log.info("Firebase sdk initialized and attached.")
 
     @app.on_event("shutdown")
     def _delete_app():
         nonlocal _app
-        delete_app(_app)
-        del _app
+        del app.dependency_overrides[firebase_app]
+        try:
+            delete_app(_app)
+            _app = None
+        except Exception as ex:
+            log.exception("Error while deleting the firebase app.", exc_info=ex)
 
-    app.dependency_overrides[App] = lambda: _app
+
+def firebase_app():
+    """Return the initialized firebase app instance.
+
+    Raises:
+        NotInitializedError: If the firebase SDK is not initialized yet.
+
+    Returns:
+        App: The firebase app instance.
+    """
+    raise NotInitializedError("The firebase application was not initialized.")
+
+
+def _app_name(app: FastAPI):
+    return f"fb_app_{hash(app)}"
