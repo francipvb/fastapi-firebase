60e4d00d4963a1e18d4c10f64933a549b250fc91
Francisco Del Roio <francipvb@hotmail.com>
2022-02-03 12:05:57 -0300

    Changed the way the token is passed to the publish command.

--- .github/scripts/publish.sh
+++ .github/scripts/publish.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
 poetry config repositories.destrepo "${REPO_URL}"
-poetry config pypi-token.destrepo "${PYPI_TOKEN}"
 
 poetry publish \
     --build \
-    --repository "destrepo"
+    --repository="destrepo" \
+    --username="__token__" \
+    --password="${PYPI_TOKEN}"
